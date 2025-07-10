f7af18862dde8555af4ce33d93b9e976dea9035a
Nabeela Kanwal <codewithnabeela@gmail.com>
2025-07-10 07:30:56 +0500

    Integrate admin theme

--- routes/admin.php
+++ routes/admin.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 
-Route::get('/dashboard', [AuthController::class, 'index'])->name('dashborad');
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [AuthController::class, 'index'])->name('dashboard');
+});
