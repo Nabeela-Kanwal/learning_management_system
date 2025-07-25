e6890c21278e5ec858f2f790d0f0fd9d4265a795
Nabeela Kanwal <codewithnabeela@gmail.com>
2025-07-25 21:22:17 +0500

    Implement category management with create functionality, including form validation and migration setup

--- resources/views/backend/admin/categories/index.blade.php
+++ resources/views/backend/admin/categories/index.blade.php
@@ -6,20 +6,21 @@
             <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Manage Categories</span> /Categories</h4>
             <!-- Bordered Table -->
             <div class="card">
-                  <div class="card-header d-flex justify-content-between align-items-center">
-                        <h5 class="mb-0">Categories</h5>
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-primary">
-                            <i class="bx bx-plus"></i> Add Category
-                        </a>
-                    </div>
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h5 class="mb-0">Categories</h5>
+                    <a href="{{ route('admin.category.create') }}" class="btn btn-primary">
+                        <i class="bx bx-plus"></i> Add Category
+                    </a>
+                </div>
                 <div class="card-body">
                     <div class="table-responsive text-nowrap">
                         <table class="table table-bordered">
                             <thead>
                                 <tr>
-                                    <th>Project</th>
-                                    <th>Client</th>
-                                    <th>Users</th>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Slug</th>
+                                    <th>Image</th>
                                     <th>Status</th>
                                     <th>Actions</th>
                                 </tr>
@@ -27,8 +28,12 @@
                             <tbody>
                                 <tr>
                                     <td>
-                                        <i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Angular
-                                            Project</strong>
+                                        <i class="fab fa-angular fa-lg text-danger me-3"></i>
+                                        <strong>1</strong>
+                                    </td>
+                                    <td>
+                                        <i class="fab fa-angular fa-lg text-danger me-3"></i>
+                                        <strong>Angular Project</strong>
                                     </td>
                                     <td>Albert Cook</td>
                                     <td>
@@ -67,9 +72,16 @@
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td><i class="fab fa-react fa-lg text-info me-3"></i> <strong>React Project</strong>
+                                <tr>
+                                    <td>
+                                        <i class="fab fa-angular fa-lg text-danger me-3"></i>
+                                        <strong>1</strong>
                                     </td>
-                                    <td>Barry Hunter</td>
+                                    <td>
+                                        <i class="fab fa-angular fa-lg text-danger me-3"></i>
+                                        <strong>Angular Project</strong>
+                                    </td>
+                                    <td>Albert Cook</td>
                                     <td>
                                         <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
                                             <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
@@ -89,7 +101,7 @@
                                             </li>
                                         </ul>
                                     </td>
-                                    <td><span class="badge bg-label-success me-1">Completed</span></td>
+                                    <td><span class="badge bg-label-primary me-1">Active</span></td>
                                     <td>
                                         <div class="dropdown">
                                             <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
@@ -105,90 +117,49 @@
                                         </div>
                                     </td>
                                 </tr>
-                                <tr>
-                                    <td><i class="fab fa-vuejs fa-lg text-success me-3"></i> <strong>VueJs Project</strong>
-                                    </td>
-                                    <td>Trevor Baker</td>
-                                    <td>
-                                        <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
-                                                class="avatar avatar-xs pull-up" title="Lilian Fuller">
-                                                <img src="../assets/img/avatars/5.png" alt="Avatar"
-                                                    class="rounded-circle" />
-                                            </li>
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                title="Sophia Wilkerson">
-                                                <img src="../assets/img/avatars/6.png" alt="Avatar"
-                                                    class="rounded-circle" />
-                                            </li>
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                title="Christina Parker">
-                                                <img src="../assets/img/avatars/7.png" alt="Avatar"
-                                                    class="rounded-circle" />
-                                            </li>
-                                        </ul>
-                                    </td>
-                                    <td><span class="badge bg-label-info me-1">Scheduled</span></td>
-                                    <td>
-                                        <div class="dropdown">
-                                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                                data-bs-toggle="dropdown">
-                                                <i class="bx bx-dots-vertical-rounded"></i>
-                                            </button>
-                                            <div class="dropdown-menu">
-                                                <a class="dropdown-item" href="javascript:void(0);"><i
-                                                        class="bx bx-edit-alt me-1"></i> Edit</a>
-                                                <a class="dropdown-item" href="javascript:void(0);"><i
-                                                        class="bx bx-trash me-1"></i> Delete</a>
-                                            </div>
+                                <td>
+                                    <i class="fab fa-angular fa-lg text-danger me-3"></i>
+                                    <strong>1</strong>
+                                </td>
+                                <td>
+                                    <i class="fab fa-angular fa-lg text-danger me-3"></i>
+                                    <strong>Angular Project</strong>
+                                </td>
+                                <td>Albert Cook</td>
+                                <td>
+                                    <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            class="avatar avatar-xs pull-up" title="Lilian Fuller">
+                                            <img src="../assets/img/avatars/5.png" alt="Avatar"
+                                                class="rounded-circle" />
+                                        </li>
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            class="avatar avatar-xs pull-up" title="Sophia Wilkerson">
+                                            <img src="../assets/img/avatars/6.png" alt="Avatar"
+                                                class="rounded-circle" />
+                                        </li>
+                                        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top"
+                                            class="avatar avatar-xs pull-up" title="Christina Parker">
+                                            <img src="../assets/img/avatars/7.png" alt="Avatar"
+                                                class="rounded-circle" />
+                                        </li>
+                                    </ul>
+                                </td>
+                                <td><span class="badge bg-label-primary me-1">Active</span></td>
+                                <td>
+                                    <div class="dropdown">
+                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                            data-bs-toggle="dropdown">
+                                            <i class="bx bx-dots-vertical-rounded"></i>
+                                        </button>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="javascript:void(0);"><i
+                                                    class="bx bx-edit-alt me-1"></i> Edit</a>
+                                            <a class="dropdown-item" href="javascript:void(0);"><i
+                                                    class="bx bx-trash me-1"></i> Delete</a>
                                         </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <i class="fab fa-bootstrap fa-lg text-primary me-3"></i> <strong>Bootstrap
-                                            Project</strong>
-                                    </td>
-                                    <td>Jerry Milton</td>
-                                    <td>
-                                        <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                title="Lilian Fuller">
-                                                <img src="../assets/img/avatars/5.png" alt="Avatar"
-                                                    class="rounded-circle" />
-                                            </li>
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                title="Sophia Wilkerson">
-                                                <img src="../assets/img/avatars/6.png" alt="Avatar"
-                                                    class="rounded-circle" />
-                                            </li>
-                                            <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
-                                                data-bs-placement="top" class="avatar avatar-xs pull-up"
-                                                title="Christina Parker">
-                                                <img src="../assets/img/avatars/7.png" alt="Avatar"
-                                                    class="rounded-circle" />
-                                            </li>
-                                        </ul>
-                                    </td>
-                                    <td><span class="badge bg-label-warning me-1">Pending</span></td>
-                                    <td>
-                                        <div class="dropdown">
-                                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
-                                                data-bs-toggle="dropdown">
-                                                <i class="bx bx-dots-vertical-rounded"></i>
-                                            </button>
-                                            <div class="dropdown-menu">
-                                                <a class="dropdown-item" href="javascript:void(0);"><i
-                                                        class="bx bx-edit-alt me-1"></i> Edit</a>
-                                                <a class="dropdown-item" href="javascript:void(0);"><i
-                                                        class="bx bx-trash me-1"></i> Delete</a>
-                                            </div>
-                                        </div>
-                                    </td>
+                                    </div>
+                                </td>
                                 </tr>
                             </tbody>
                         </table>
