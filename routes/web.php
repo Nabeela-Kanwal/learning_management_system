efec3e38974d5e4471b04cd34c180627b2eb1bee
Nabeela Kanwal <codewithnabeela@gmail.com>
2025-08-25 16:26:33 +0500

    Added front-end layout and header components for the LMS, including a new header view, main layout with styles/scripts, and updated web routes to use HomeController.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 
 
