d6ef1405801801bd5ad9563cca766038c37f419f
Nabeela Kanwal <codewithnabeela@gmail.com>
2025-09-03 20:45:02 +0500

    Refactor HomeController to include banner retrieval for the homepage and clean up home.blade.php by removing unused hero slider items

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -11,9 +12,10 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::where('status', 1)->get();
-
+        $banners = Banner::where('page', 'home')->where('status', 1)->get();
         return view('frontend.home', [
             'categories' => $categories,
+            'banners' => $banners,
         ]);
     }
 }
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -24,53 +24,9 @@
                     </div>
                 </div>
             </div>
-            <div class="hero-slider-item hero-bg-2">
-                <div class="container">
-                    <div class="hero-content text-center">
-                        <div class="section-heading">
-                            <h2 class="section__title text-white fs-65 lh-80 pb-3">Join Aduca & Get <br> Your Free
-                                Courses!</h2>
-                            <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting
-                                industry orem Ipsum has been the
-                                <br>industry's standard dummy text ever sinceprinting and typesetting industry.
-                            </p>
-                        </div>
-                        <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-center">
-                            <a href="admission.html" class="btn theme-btn mr-4 mb-4">Get Started <i
-                                    class="la la-arrow-right icon ml-1"></i></a>
-                            <a href="#" class="btn-text video-play-btn mb-4" data-fancybox
-                                data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
-                                Watch Preview<i class="la la-play icon-btn ml-2"></i>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="hero-slider-item hero-bg-3">
-                <div class="container">
-                    <div class="hero-content text-right">
-                        <div class="section-heading">
-                            <h2 class="section__title text-white fs-65 lh-80 pb-3">Learn Anything, <br> Anytime,
-                                Anywhere</h2>
-                            <p class="section__desc text-white pb-4">Emply dummy text of the printing and typesetting
-                                industry orem Ipsum has been the
-                                <br>industry's standard dummy text ever sinceprinting and typesetting industry.
-                            </p>
-                        </div>
-                        <div class="hero-btn-box d-flex flex-wrap align-items-center pt-1 justify-content-end">
-                            <a href="#" class="btn-text video-play-btn mr-4 mb-4" data-fancybox
-                                data-src="https://www.youtube.com/watch?v=cRXm1p-CNyk">
-                                <i class="la la-play icon-btn mr-2"></i>Watch Preview
-                            </a>
-                            <a href="admission.html" class="btn theme-btn mb-4"><i
-                                    class="la la-arrow-left icon mr-1"></i>Get Enrolled </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </section>
-    < <section class="feature-area pb-90px">
+     <section class="feature-area pb-90px">
         @include('components.featured.feature-section')
         </section>
 
