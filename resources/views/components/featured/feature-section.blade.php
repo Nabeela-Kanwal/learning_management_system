45a9b5c77439d5dc354965356f5dc1088281102b
Nabeela Kanwal <96948266+Nabeela-Kanwal@users.noreply.github.com>
2026-09-17 10:01:54 +0500

    feat: add responsive info cards styling; implement new layout and design for info section

--- resources/views/components/featured/feature-section.blade.php
+++ resources/views/components/featured/feature-section.blade.php
@@ -1,26 +1,19 @@
 @if ($infos->isNotEmpty())
-    <style>
-        @media (min-width: 992px) {
-            .feature-content-wrap > .info-card-column {
-                flex: 0 0 20%;
-                max-width: 20%;
-            }
-        }
-    </style>
+    @once
+        @push('styles')
+            <link rel="stylesheet" href="{{ asset('frontend/css/info-cards.css') }}">
+        @endpush
+    @endonce
     <div class="container-fluid px-3 px-lg-4">
-        <div class="row feature-content-wrap">
+        <div class="learning-benefits">
             @foreach ($infos as $info)
-                <div class="col-12 col-md-6 info-card-column mb-4">
-                    <div class="info-box h-100">
-                        <div class="info-overlay"></div>
-                        <div class="icon-element mx-auto shadow-sm">
-                            <i class="la {{ $info->icon }} fs-40" style="color: #e0007b" aria-hidden="true"></i>
-                        </div>
-                        <h3 class="info__title">{{ $info->title }}</h3>
-                        <p class="info__text" style="white-space: pre-line; overflow-wrap: anywhere">
-                            {{ $info->description }}</p>
+                <article class="learning-benefit">
+                    <div class="learning-benefit__icon">
+                        <i class="la {{ $info->icon }}" aria-hidden="true"></i>
                     </div>
-                </div>
+                    <h3 class="learning-benefit__title">{{ $info->title }}</h3>
+                    <p class="learning-benefit__text">{{ $info->description }}</p>
+                </article>
             @endforeach
         </div>
     </div>
